704c685fc1e622bfb089b0ead1248d58789b11dc
Lalit <lalitnayyar@gmail.com>
2025-01-01 06:27:19 +0400

    challange code
    /**
     * Super Challenge:
     *
     * Set up a RunnableSequence so that the standaloneQuestionPrompt
     * passes the standalone question to the retriever, and the retriever
     * passes the combined docs as context to the answerPrompt. Remember,
     * the answerPrompt should also have access to the original question.
     *
     * When you have finished the challenge, you should see a
     * conversational answer to our question in the console.
     *
    **/

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { PromptTemplate } from 'langchain/prompts'
 import { StringOutputParser } from 'langchain/schema/output_parser'
 import { retriever } from '/utils/retriever'
+import { combineDocuments } from '/utils/combineDocuments'
+import { RunnablePassthrough, RunnableSequence } from "langchain/schema/runnable"
 
 document.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -12,22 +14,28 @@ const openAIApiKey = process.env.OPENAI_API_KEY
 const llm = new ChatOpenAI({ openAIApiKey })
 
 const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
-
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
 const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
 context: {context}
 question: {question}
-answer: 
-`
-
+answer: `
 const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
 
-function combineDocuments(docs){
-    return docs.map((doc)=>doc.pageContent).join('\n\n')
-}
+/**
+ * Super Challenge:
+ * 
+ * Set up a RunnableSequence so that the standaloneQuestionPrompt 
+ * passes the standalone question to the retriever, and the retriever
+ * passes the combined docs as context to the answerPrompt. Remember, 
+ * the answerPrompt should also have access to the original question. 
+ * 
+ * When you have finished the challenge, you should see a 
+ * conversational answer to our question in the console.
+ * 
+**/
 
-const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever).pipe(combineDocuments)
+const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever).pipe(combineDocuments).pipe(answerPrompt)
 
 const response = await chain.invoke({
     question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'

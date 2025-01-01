27f93c6d57fc3159c0ff08e74fc429775824af6c
Lalit <lalitnayyar@gmail.com>
2025-01-01 07:00:42 +0400

    update code with conversation history in question and answer
    try testing My Name is Lalit and what is scrimba?
    Next question what is my name
    you will get answer Lalit

--- index.js
+++ index.js
@@ -29,13 +29,19 @@ document.addEventListener('submit', (e) => {
 const openAIApiKey = process.env.OPENAI_API_KEY
 const llm = new ChatOpenAI({ openAIApiKey })
 
-const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+const standaloneQuestionTemplate = `Given some conversation history (if any) and a question, convert the question to a standalone question. 
+conversation history: {conv_history}
+question: {question} 
+standalone question:`
+
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided and the conversation history. Try to find the answer in the context. If the answer is not given in the context, find the answer in the conversation history if possible. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
 context: {context}
+conversation history: {conv_history}
 question: {question}
 answer: `
+
 const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
 
 const standaloneQuestionChain = standaloneQuestionPrompt
@@ -58,7 +64,8 @@ const chain = RunnableSequence.from([
     },
     {
         context: retrieverChain,
-        question: ({ original_input }) => original_input.question
+        question: ({ original_input }) => original_input.question,
+        conv_history: ({ original_input }) => original_input.conv_history
     },
     answerChain
 ])
@@ -78,7 +85,8 @@ async function progressConversation() {
     newHumanSpeechBubble.textContent = question
     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
     const response = await chain.invoke({
-        question: question
+        question: question,
+        conv_history: formatConvHistory(convHistory)
     })
 
     convHistory.push(question)
